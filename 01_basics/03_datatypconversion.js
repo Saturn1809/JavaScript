b4e7f282a48b02daf5de3a66f299ac7356af2f4a
Vipin Yadav <156163889+Saturn1809@users.noreply.github.com>
2024-05-26 13:57:45 +0000

    conversion

--- 01_basics/03_datatypconversion.js
+++ 01_basics/03_datatypconversion.js
@@ -40,12 +40,44 @@ let stringNumber = String(someNumber);
 console.log(stringNumber);
 console.log(typeof stringNumber);
 
-
-
-
-
-
-
-
-
 //node 01_basics/03_datatypconversion.js
+
+//******************** Operations ********************* */
+
+let value = 3
+let negValue = -value 
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+
+let str1= "Hello"
+let str2= "vipin"
+str3=str1 + str2
+console.log(str3);
+
+
+
+console.log("1"+ 2);
+console.log(1 +"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+
+//tricky conversion 
+
+console.log(true);
+console.log(+true); // Do not use it
+console.log(+""); // Do not use it
+
+let num1 ,num2,num3 // Do not use it
+num1=num2=num3=2 + 2
+
+let gameCounter=100
+gameCounter++;
+++gameCounter;
+console.log(gameCounter);
+
