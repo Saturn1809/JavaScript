d834ca690f0173b9816e975eee053156ffd460bb
Vipin Yadav <156163889+Saturn1809@users.noreply.github.com>
2024-05-26 13:22:02 +0000

    coversion of data types

---
+++ 01_basics/03_datatypconversion.js
@@ -0,0 +1,51 @@
+let score ="33ABC"
+let score1=null
+
+console.log(typeof (score));
+
+let valueInNumber = Number(score)  //used for changing type of variables 
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+// chcek if it's giving NaN(Not a Number) then the no is not a pure no
+
+console.log(typeof (score1));
+
+let valueInNumber1 = Number(score1)  //used for changing type of variables 
+console.log(typeof valueInNumber1);
+console.log(valueInNumber1);
+
+// "33" = 33
+// "33abc" = NaN
+// trye = 1; false = 0
+
+let isLoggedin1 = 1 //True 
+let isLoggedin2 = 0 //False 
+let isLoggedin3 = "" //False
+let isLoggedin4 = "Vipin" //True
+
+let booleanIsLoggedIn1 = Boolean(isLoggedin1)
+console.log(booleanIsLoggedIn1);
+
+let booleanIsLoggedIn2 = Boolean(isLoggedin2)
+console.log(booleanIsLoggedIn2);
+
+let booleanIsLoggedIn3 = Boolean(isLoggedin3)
+console.log(booleanIsLoggedIn3);
+
+let booleanIsLoggedIn4 = Boolean(isLoggedin4)
+console.log(booleanIsLoggedIn4);
+
+let someNumber = 33
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
+
+
+
+
+
+//node 01_basics/03_datatypconversion.js
