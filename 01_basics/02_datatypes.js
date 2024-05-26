fbb33fe9a3a7bc510a81183cd5494446e5a76611
Vipin Yadav <156163889+Saturn1809@users.noreply.github.com>
2024-05-26 10:48:05 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all JS code as newer version 
+
+// alert(3+3) // we are using node js, not browser
+
+//Code readability should by high 
+//Ecma Scrip(tc39) and mdn  is for JS Documentation 
+//node 01_basics/02_datatypes.js
+
+let name="Vipin" // String
+let age=18  // int or number 
+let isLoggedIn = false //boolean
+let state; //undefined 
+
+// null = it's an standalone value(Representation of empty value)
+// undefined = when value is not assigned 
+// symbol = for finding uniqueness 
+
+// object 
+console.log(typeof undefined); //type is undefined
+console.log(typeof null); // type is object
