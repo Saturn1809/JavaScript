dda13bc8a98b620cfbda327b0c5e656fb0618211
Vipin Yadav <156163889+Saturn1809@users.noreply.github.com>
2024-06-02 06:01:42 +0000

    Switch Case

---
+++ 04_Controlflow/switch.js
@@ -0,0 +1,26 @@
+// node 04_Controlflow/switch.js
+
+
+const month = 3
+
+switch (month) {
+    case 1:
+        
+        console.log("January");
+        break;  // --> breaks control flow
+    case 2:
+        
+        console.log("February");
+        break;
+    case 3:
+        
+        console.log("March");
+        break;
+    case 4:
+        
+        console.log("April");
+        break;
+
+    default:
+        break;
+}
